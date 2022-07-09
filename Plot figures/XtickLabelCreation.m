5390174bba09a22df5740ae78aaaf57efce12366
Chenhui Ma <68667446+Chenhui88569@users.noreply.github.com>
2022-07-09 14:52:07 -0700

    Delete Plot figures directory

--- Plot figures/XtickLabelCreation.m
+++
@@ -1,8 +0,0 @@
-function [t_xtick, t_xlabel] = XtickLabelCreation(t_list, t_list_label)
-%the unit of the elements in t_list is min
-t_xtick = reshape(t_list,[1,size(t_list,1)]);
-t_xlabel = reshape(t_list_label ,[1,size(t_list,1)]) ;
-t_xlabel = round(t_xlabel,2);
-t_xlabel  = num2cell(t_xlabel,1);
-end
-
